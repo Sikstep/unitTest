9d8964b8a2607df5d6d52d394ada2183212b5959
Sikstep <Sikstep1988@gmail.com>
2023-02-07 10:55:15 +0300

    NewUnitTest lesson 2 Object

---
+++ src/01_Hello_test/02_02.ts
@@ -0,0 +1,29 @@
+export type CityType = {
+    title: string
+    houses: Array<HousesType>
+    govermentBuilding: Array<GovermentType>
+    citizensNumber: number
+    }
+
+    export type GovermentType = {
+        type: 'HOSPITAL' | 'FIRE-STATION'
+        budget: number
+        staffCount: number
+        address: AddressType
+    }
+
+
+export type HousesType = {
+    buildedAt: number
+    repaired: boolean
+    address: AddressType
+}
+
+export type AddressType = {
+    number?: number
+    street: StreetType
+}
+
+export type StreetType = {
+    title: string
+}
---
+++ src/01_Hello_test/03.ts
@@ -0,0 +1,9 @@
+debugger
+export const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+// export const addSkill = (student: StudentType, skill:string) =>
+// {
+//
+// }
