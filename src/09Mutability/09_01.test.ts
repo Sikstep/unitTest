04756c0ff047c22f50e8bba065002dd6b20741b9
Sikstep <Sikstep1988@gmail.com>
2023-03-01 13:15:34 +0300

    09 Mutability start++

--- src/09Mutability/09_01.test.ts
+++ src/09Mutability/09_01.test.ts
@@ -1,11 +1,11 @@
-import { userType } from "./09_01";
+import {userType} from './09_01';
 
 
 const icreaseAge = (us: userType) => {
     us.age++;
 }
 
-test('big test', () => {
+test('reference type test', () => {
     let user = {
         name: 'Dimych',
         age: 32,
@@ -13,4 +13,33 @@ test('big test', () => {
     icreaseAge(user)
 
     expect(user.age).toBe(33);
+
+    const superman = user
+
+    superman.age = 1000;
+
+    expect(user.age).toBe(1000);
+
+})
+
+test('array test', () => {
+    let users = [
+        {
+            name: 'Dimych',
+            age: 32,
+        },
+        {
+            name: 'Dimych',
+            age: 32,
+        },
+    ]
+
+    let admins = users;
+
+    admins.push({name: 'Bandugan', age: 10})
+
+    expect(users[2]).toEqual({name: 'Bandugan', age: 10});
+
+
+
 })
